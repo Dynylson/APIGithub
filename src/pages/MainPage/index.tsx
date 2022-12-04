de9e9687e266228d0c53b21a337ed777d807e8d7
Dynylson <dynylsonjr@gmail.com>
2022-12-04 16:26:52 -0300

    Configuracao das rotas

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,17 +1,22 @@
+import { useState } from 'react';
+
 import { Container, Form, Logo, Title, Input, Button } from './styles'
 
 import { MdSearch } from 'react-icons/md';
 
 import githubLogo from '../../assets/images/github-logo.svg';
 
+
 const MainPage = () => {
+  const [login, setLogin] = useState('');
+
   return (
     <Container>
       <Logo src={githubLogo} alt='Github Logo' />
       <Title>API Github</Title>
       <Form>
-        <Input placeholder='Usuário'/>
-        <Button>
+        <Input placeholder='Usuário' value={login} onChange={(event) => setLogin(event.target.value)} />
+        <Button to={`/${login}/repositories`}>
           <MdSearch size={42} color='#fff' />
         </Button>
       </Form>
---
+++ src/pages/RepositoriesPage/index.tsx
@@ -0,0 +1,7 @@
+const RepositoriesPage = () => {
+  return (
+    <h1>Repositories</h1>
+  )
+}
+
+export default RepositoriesPage;
