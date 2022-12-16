eb0b5b61adcf6fd77540e5552a178af40b05f8b8
Dynylson <dynylsonjr@gmail.com>
2022-12-16 10:13:20 -0300

    Carregando repositorios

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -8,103 +8,30 @@ import Repositories from './Repositories'
 import { Loading, Container, Sidebar, Main } from './styles'
 
 import { langColors } from '../../services/config'
-import { getUser } from '../../services/api'
+import { getUser, getRepos, getLangsFrom } from '../../services/api'
 
 const RepositoriesPage = () => {
   const [user, setUser] = useState()
+  const [repositories, setRepositories] = useState()
+  const [languages, setLanguages] = useState()
   const [currentLanguage, setCurrentLanguage] = useState()
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const loadData = async () => {
-      // getUser()
-
-      const [userResponse] = await Promise.all([getUser('Dynylson')])
-
+      const [userResponse, repositoriesResponse] = await Promise.all([
+        getUser('Dynylson'),
+        getRepos('Dynylson'),
+      ])
       setUser(userResponse.data)
+      setRepositories(repositoriesResponse.data)
+      setLanguages(getLangsFrom(repositoriesResponse.data))
 
       setLoading(false)
     }
 
     loadData()
   }, [])
-
-  const user2 = {
-    login: 'Dynylson',
-    name: 'Dynylson Junior',
-    avatar_url: 'https://avatars.githubusercontent.com/u/94336147?v=4',
-    followers: 9,
-    following: 18,
-    company: 'ETE - PE',
-    blog: '',
-    location: 'Brazil',
-  }
-
-  const repositories = [
-    {
-      id: 1,
-      name: 'Repo 1',
-      description: 'Descricao',
-      html_url: 'https://github.com/Dynylson',
-      language: 'JavaScript',
-    },
-    {
-      id: 2,
-      name: 'Repo 2',
-      description: 'Descricao',
-      html_url: 'https://github.com/Dynylson',
-      language: 'JavaScript',
-    },
-    {
-      id: 3,
-      name: 'Repo 3',
-      description: 'Descricao',
-      html_url: 'https://github.com/Dynylson',
-      language: 'PHP',
-    },
-    {
-      id: 4,
-      name: 'Repo 4',
-      description: 'Descricao',
-      html_url: 'https://github.com/Dynylson',
-      language: 'Ruby',
-    },
-    {
-      id: 5,
-      name: 'Repo 5',
-      description: 'Descricao',
-      html_url: 'https://github.com/Dynylson',
-      language: 'TypeScript',
-    },
-    {
-      id: 6,
-      name: 'Repo 6',
-      description: 'Descricao',
-      html_url: 'https://github.com/Dynylson',
-      language: 'Ruby',
-    },
-  ]
-
-  let stats = repositories
-    .map((repository) => repository.language)
-    .reduce(
-      (data, language) => ({
-        ...data,
-        [language]: (data[language] || 0) + 1,
-      }),
-      [],
-    )
-
-  delete stats.null
-
-  stats = Object.keys(stats)
-    .map((language) => ({
-      name: language,
-      count: stats[language],
-      color: langColors[language.toLowerCase()],
-    }))
-    .sort((a, b) => b.count - a.count)
-  console.log(stats)
 
   // Calculo dos filters
 
@@ -127,7 +54,7 @@ const RepositoriesPage = () => {
           <Profile user={user} />
         </ProfileContextProvider>
         <Filter
-          languages={stats}
+          languages={languages}
           currentLanguage={currentLanguage}
           onClick={onFilterClick}
           onClickClearFilter={onClickClearFilter}
--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-})
-
-export const getUser = async (login: string) => api.get(`/users/${login}`)
-
-export default api
